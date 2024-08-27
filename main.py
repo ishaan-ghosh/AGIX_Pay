97de4c19c4e43e3d34922851497ea2485b9b8628
ishaan-ghosh <ishaanghosh01@gmail.com>
2024-08-27 16:59:15 +0400

    Added info from SNET, fixed checksum conversion for addresses, added basic error handling

--- main.py
+++ main.py
@@ -1,55 +1,84 @@
 import os
+import json
 from web3 import Web3
 from eth_account import Account
 from dotenv import load_dotenv
 
 load_dotenv()
 w3 = Web3(Web3.HTTPProvider(os.getenv('SEPOLIA_NODE_URL')))
-AGIX_CONTRACT_ADDRESS = os.getenv('AGIX_CONTRACT_ADDR')  
-# Needed from SNET team
-AGIX_ABI = []
+AGIX_CONTRACT_ADDRESS = Web3.to_checksum_address(os.getenv('AGIX_CONTRACT_ADDR'))
+AGIX_ABI = json.loads('''[{"inputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"symbol","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"account","type":"address"}],"name":"Paused","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":true,"internalType":"bytes32","name":"previousAdminRole","type":"bytes32"},{"indexed":true,"internalType":"bytes32","name":"newAdminRole","type":"bytes32"}],"name":"RoleAdminChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":true,"internalType":"address","name":"account","type":"address"},{"indexed":true,"internalType":"address","name":"sender","type":"address"}],"name":"RoleGranted","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"role","type":"bytes32"},{"indexed":true,"internalType":"address","name":"account","type":"address"},{"indexed":true,"internalType":"address","name":"sender","type":"address"}],"name":"RoleRevoked","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"account","type":"address"}],"name":"Unpaused","type":"event"},{"inputs":[],"name":"DEFAULT_ADMIN_ROLE","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"MINTER_ROLE","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"PAUSER_ROLE","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burn","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burnFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"}],"name":"getRoleAdmin","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"getRoleMember","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"}],"name":"getRoleMemberCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"grantRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"hasRole","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"paused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"renounceRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"role","type":"bytes32"},{"internalType":"address","name":"account","type":"address"}],"name":"revokeRole","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"pause","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"unpause","outputs":[],"stateMutability":"nonpayable","type":"function"}]''')
 from_private_key = os.getenv('FROM_PRIVATE_KEY')
 
 
 
 
 # NOTE: SET DELIVERY WALLET ADDRESS
-to_address = ''
+to_address = Web3.to_checksum_address('') 
 
 # NOTE: SET TRANSACTION AMOUNT IN AGIX TOKENS
-amount = Web3.to_wei(1, 'ether')
-
+amount = 1  # This will transfer 1 AGIX
 
 
 
 
 def transfer_agix(from_private_key, to_address, amount):
-    account = Account.from_key(from_private_key)
-    contract = w3.eth.contract(address=AGIX_CONTRACT_ADDRESS, abi=AGIX_ABI)
-    nonce = w3.eth.get_transaction_count(account.address)
+    try:
+        account = Account.from_key(from_private_key)
+        contract = w3.eth.contract(address=AGIX_CONTRACT_ADDRESS, abi=AGIX_ABI)
 
-    estimated_gas = contract.functions.transfer(to_address, amount).estimate_gas({
-        'from': account.address,
-        'nonce': nonce,
-    })
+        token_decimals = contract.functions.decimals().call() 
+        print(f"AGIX Token Decimals: {token_decimals}")
 
-    gas_limit = int(estimated_gas * 1.2)
+        balance = contract.functions.balanceOf(account.address).call() 
+        print(f"AGIX Balance: {balance / (10 ** token_decimals)} AGIX")
 
-    tx = contract.functions.transfer(to_address, amount).build_transaction({
-        'from': account.address,
-        'nonce': nonce,
-        'gas': gas_limit, 
-        'gasPrice': w3.eth.gas_price,
-        'chainId': 11155111  
-    })
+        amount_in_smallest_unit = int(amount * (10 ** token_decimals))
+        print(f"Attempting to transfer: {amount} AGIX ({amount_in_smallest_unit} smallest units)")
 
-    signed_tx = account.sign_transaction(tx)
-    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        if balance < amount_in_smallest_unit:
+            print(f"Insufficient balance. You're trying to send {amount} AGIX, but your balance is {balance / (10 ** token_decimals)} AGIX")
+            return None
 
-    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-    
-    return tx_receipt
+        nonce = w3.eth.get_transaction_count(account.address)
+
+        estimated_gas = contract.functions.transfer(to_address, amount_in_smallest_unit).estimate_gas({
+            'from': account.address,
+            'nonce': nonce,
+        })
+
+        gas_limit = int(estimated_gas * 1.2)
+
+        tx = contract.functions.transfer(to_address, amount_in_smallest_unit).build_transaction({
+            'from': account.address,
+            'nonce': nonce,
+            'gas': gas_limit, 
+            'gasPrice': w3.eth.gas_price,
+            'chainId': 11155111  
+        })
+
+        signed_tx = account.sign_transaction(tx)
+
+        tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
+        tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+        return tx_receipt
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        print("Exception details:", e.__class__.__name__)
+        import traceback
+        traceback.print_exc()
+        return None
 
 if __name__ == "__main__":
-    receipt = transfer_agix(from_private_key, to_address, amount)
-    print(f"Transaction successful. Transaction hash: {receipt['transactionHash'].hex()}")
+    if not w3.is_connected():
+        print("Not connected to Ethereum network")
+    else:
+        print("Connected to Ethereum network")
+        print(f"AGIX Contract Address: {AGIX_CONTRACT_ADDRESS}")
+        print(f"To Address: {to_address}")
+        
+        receipt = transfer_agix(from_private_key, to_address, amount)
+        if receipt:
+            print(f"Transaction successful. Transaction hash: {receipt['transactionHash'].hex()}")
+        else:
+            print("Transaction failed")
